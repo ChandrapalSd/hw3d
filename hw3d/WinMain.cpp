bb98517d6d9fe4a9f82f5e3fc4a45fef98f9bb1c
Chandrapal Singh <chandrapalsd@outlook.com>
2022-12-31 11:03:55 +0530

    [Window Framework] 6

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,47 +1,5 @@
-#include <Windows.h>
-#include "WindowsMessageMap.h"
-#include <sstream>
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	static WindowsMessageMap mm;
-	OutputDebugString(mm(msg, lParam, wParam).c_str());
-
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(69);
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'D')
-		{
-			SetWindowText(hWnd, "Respects");
-		}
-		break;
-	case WM_KEYUP:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "Dangerfield");
-		}
-		break;
-	case WM_CHAR:
-		{
-			static std::string title;
-			title.push_back((char)wParam);
-			SetWindowText(hWnd, title.c_str());
-		}
-		break;
-	case WM_LBUTTONDOWN:
-		{
-			POINTS pt = MAKEPOINTS(lParam);
-			std::stringstream oss;
-			oss << "(" << pt.x << "," << pt.y << ")";
-			SetWindowText(hWnd, oss.str().c_str());
-		}
-		break;
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -50,39 +8,10 @@ int CALLBACK WinMain(
 	int		  nCmdShow
 	)
 {
-	const char* pClassName = "hw3dbutts";
+	Window wnd(800, 300, "Donkey Fart Box");
 
-	// register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
-
-	// create window instance
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		"Happy Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-		);
-
-	// show the window
-	ShowWindow(hWnd, SW_SHOW);
-
-	// message pump
-	MSG msg = { 0 };
-	BOOL gResult = 0;
+	MSG msg;
+	BOOL gResult;
 	while ( (gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
 		TranslateMessage(&msg);
@@ -93,9 +22,7 @@ int CALLBACK WinMain(
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
+
+	return msg.wParam;
 	
 }
