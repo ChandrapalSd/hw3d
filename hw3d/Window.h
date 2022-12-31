90b41fd4cca25b773a13dd6c2295ddca4940f121
Chandrapal Singh <chandrapalsd@outlook.com>
2022-12-31 13:21:25 +0530

    [Custom Icons / Error Handling] 7

--- hw3d/Window.h
+++ hw3d/Window.h
@@ -1,9 +1,24 @@
 #pragma once
 #include "ChiliWin.h"
+#include "ChiliException.h"
 
 
 class Window
 {
+public:
+	class Exception : public ChiliException
+	{
+	public:
+		Exception(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		virtual const char* GetType() const noexcept;
+		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+	private:
+		HRESULT hr;
+	};
+
 private:
 	// singleton manages registration/cleanup of window class
 	class WindowClass
@@ -22,7 +37,7 @@ private:
 	};
 
 public:
-	Window(int width, int height, const char* name) noexcept;
+	Window(int width, int height, const char* name);
 	~Window();
 	Window(const Window&) = delete;
 	Window& operator=(const Window&) = delete;
@@ -35,3 +50,7 @@ private:
 	int height;
 	HWND hWnd;
 };
+
+// error exception helper macro
+#define CHWND_EXCEPT( hr ) Window::Exception( __LINE__,__FILE__,hr )
+#define CHWND_LAST_EXCEPT() Window::Exception( __LINE__,__FILE__,GetLastError() )
