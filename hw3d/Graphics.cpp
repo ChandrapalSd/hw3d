f196425949f83fd69c9d3fb49ff742b83388ec52
Chandrapal Singh <contact@chandrapal.dev>
2023-01-06 14:17:08 +0530

    Switch to DXGI flip model

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -34,10 +34,10 @@ Graphics::Graphics(HWND hWnd)
 	sd.SampleDesc.Count = 1;
 	sd.SampleDesc.Quality = 0;
 	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	sd.BufferCount = 1;
+	sd.BufferCount = 2;
 	sd.OutputWindow = hWnd;
 	sd.Windowed = TRUE;
-	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
 	sd.Flags = 0;
 
 	UINT swapCreateFlags = 0u;
